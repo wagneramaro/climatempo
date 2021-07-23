9369ead51cd27ff2a6698f882c052c8bd2001161
Wagner Amaro <36154208+wagneramaro@users.noreply.github.com>
2021-07-23 09:43:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ document.querySelector('.busca').addEventListener('submit', async(event) => {
     if (input !== '') {
         clearInfo();
         showWarning('Carregando...');
-
-        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=d06cdb298fafc83c520d5ab677fc477e&units=metric&lang=pt_br`;
+        
+        //Cadastre-se no site https://openweathermap.org e copie sua api. Geralmente ela demora 60 minutos para ficar ativa.
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=SUA_API_AQUI&units=metric&lang=pt_br`;
         let results = await fetch(url);
         let json = await results.json();
 
